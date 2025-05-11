ce70135a54bcd618ed4ab31bd4503fe21475e2c2
Gaurav Verma <gaurav123cse@gmail.com>
2025-05-12 01:13:00 +0530

    cart page complete

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createContext } from "react";
 import {products} from '../assets/assets'
+import { toast } from "react-toastify";
 
 
 export const ShopContext = createContext();
@@ -11,10 +12,77 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+
+        if(!size){
+            toast.error('Select product size');
+            return;
+        }
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] +=1;
+            }
+            else{
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{
+            cartData[itemId] ={};
+            cartData[itemId][size] =1;
+        }
+        setCartItems(cartData);
+    }
+
+    const getCartCount = () => {
+        let totalCount = 0;
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try{
+                    if(cartItems[items][item]>0){
+                        totalCount += cartItems[items][item];
+                    }
+                } catch(error){
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
+
+    const updateQuantity = async (itemId, size, quantity) => {
+
+        let cartData = structuredClone(cartItems);
+        cartData[itemId][size] = quantity;
+        setCartItems(cartData);
+    }
+
+    const getCartAmount = async () => {
+        let totalAmount =0;
+        for(const items in cartItems){
+            let itemInfo = products.find((product)=> product._id === items);
+            for(const item in cartItems[items]){
+                try{
+                    if(cartItems[items][item]>0){
+                        totalAmount += itemInfo.price * cartItems[items][item];
+                    }
+                } catch(error){
+
+                }
+            }
+        }
+        return totalAmount;
+    }
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart,
+        getCartCount, updateQuantity,
+        getCartAmount
     }
     return (
         <ShopContext.Provider value={value}>
--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -2,11 +2,12 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
   const {productId} = useParams();
-  const {products,currency} = useContext(ShopContext);
+  const {products,currency,addToCart} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
   const [size, setSize] = useState('');
@@ -66,7 +67,7 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
           <hr className='mt-8'/>
           <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
             <p>100% Original product.</p>
@@ -75,6 +76,21 @@ const Product = () => {
           </div>
         </div>
       </div>
+
+      {/* description and review section */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews(122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque similique nihil necessitatibus nulla deleniti repellat!</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid, itaque?</p>
+        </div>
+      </div>
+
+      {/* display related product */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
        
     </div>
   ) : <div className='opacity-0'></div>
