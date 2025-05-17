502661973df15e2b3713535ffb3169a09b3e899e
Gaurav Verma <gaurav123cse@gmail.com>
2025-05-18 03:43:42 +0530

    add.jsx file complete

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+     tailwindcss(),
+  ],
   server: {port : 5174}
 })
+
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
