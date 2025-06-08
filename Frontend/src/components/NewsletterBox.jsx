722b48ba5b95d1663ad11d81575858d4d2298ecb
Gopal Maurya <mauryagopal25@gmail.com>
2025-06-08 20:42:20 +0530

    every error removed

--- Frontend/src/components/NewsletterBox.jsx
+++ Frontend/src/components/NewsletterBox.jsx
@@ -9,7 +9,7 @@ const NewsletterBox = () => {
   return (
     <div className='text-center'>
         <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
-        <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet.</p>
+        <p className='text-gray-400 mt-3'></p>
 
         <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
             <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email'/>
