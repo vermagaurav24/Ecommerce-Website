1a4fa246e28a49bbf021f3b10fc4928fd6c9293f
Gopal Maurya <mauryagopal25@gmail.com>
2025-07-01 00:53:11 +0530

    mongodb setting done

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { backendUrl, currency } from '../App'
 import { toast } from 'react-toastify'
 
-const List = ({token}) => {
+const List = ({ token }) => {
 
   const [list, setList] = useState([])
 
@@ -29,15 +29,20 @@ const List = ({token}) => {
   const removeProduct = async (id) => {
     try {
 
-      const response = await axios.post(backendUrl + '/api/product/remove', {id} , {headers:{token}})
-      if(response.data.success) {
+      const response = await axios.post(backendUrl + '/api/product/remove', { id },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          }
+        })
+      if (response.data.success) {
         toast.success(response.data.message)
         await fetchList();
       }
       else {
         toast.error(response.data.message)
       }
-      
+
     } catch (error) {
       console.log(error)
       toast.error(error.message)
@@ -64,13 +69,13 @@ const List = ({token}) => {
 
         {/* --- product list--- */}
         {
-          list.map((item,index) => (
+          list.map((item, index) => (
             <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text:sm' key={index}>
               <img className='w-12' src={item.image[0]} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>{currency}{item.price}</p>
-              <p onClick={()=>removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
+              <p onClick={() => removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
             </div>
           ))
         }
